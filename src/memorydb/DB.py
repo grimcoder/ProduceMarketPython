319a74661c10c15611c5af076a0f18e251c62e09
grimcoder <papochka12>
2015-08-21 17:41:27 -0700

    added get for sales and priceChanges

--- src/memorydb/DB.py
+++ src/memorydb/DB.py
@@ -9,3 +9,25 @@ def getprices(id):
         int(i['Id']) == int(id), prices)
 
     return prices
+
+def getsales(id):
+  rowFile = open('./../data/sales.json')
+  sales = json.load(rowFile)
+  if (id):
+      return filter(
+          lambda i:
+          str(i['Id']) == id, sales)
+
+  return sales
+
+def getPriceChanges(id):
+  rowFile = open('./../data/priceChanges.json')
+  priceChanges = json.load(rowFile)
+
+  if (id):
+      return filter(
+          lambda i:
+          str(i['Id']) == id, priceChanges)
+
+  return priceChanges
+
