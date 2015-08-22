71104959bf79170895192bee5912bec006c4c192
Taras Kovtun <grimcoder@users.noreply.github.com>
2015-08-21 23:21:50 -0700

    implemented basic memorydb routines

--- src/main.py
+++ src/main.py
@@ -1,26 +1,50 @@
 from flask.ext.api import FlaskAPI
-from flask import  request
+from flask import request
 from flask.ext.cors import CORS
 
 app = FlaskAPI(__name__)
 CORS(app)
 from memorydb.DB import *
 
-@app.route("/api/prices")
+@app.route("/api/prices", methods=['GET'])
 def prices():
     id = request.args.get('id')
-    return getprices(id)
+    return MemoryDB.getprices(id)
+
+@app.route("/api/prices", methods=['POST'])
+def upsertprices():
+    data = request.data
+    MemoryDB.upsertprices(data)
+    return ''
+
+
+@app.route("/api/sales", methods=['POST'])
+def upsertsales():
+    data = request.data
+    MemoryDB.upsertsales(data)
+    return ''
+
+@app.route("/api/prices", methods=['DELETE'])
+def pricesdelete():
+    id = request.args.get('id')
+    MemoryDB.deleteprices(id)
+    return ''
 
 @app.route("/api/sales")
 def sales():
     id = request.args.get('id')
-    return getsales(id)
+    return MemoryDB.getsales(id)
 
+@app.route("/api/sales", methods=['DELETE'])
+def salesdelete():
+    id = request.args.get('id')
+    MemoryDB.deletesales(id)
+    return ''
 
 @app.route("/api/reports/prices")
-def priceChanges():
+def pricechanges():
     id = request.args.get('id')
-    return getPriceChanges(id)
+    return MemoryDB.getPriceChanges(id)
 
 if __name__ == '__main__':
     app.run(debug=True, port=3001, threaded=True)
--- src/memorydb/DB.py
+++ src/memorydb/DB.py
@@ -1,33 +1,65 @@
 import json
 
-def getprices(id):
+class MemoryDB(object):
+
     rowFile = open('./../data/prices.json')
     prices = json.load(rowFile)
-    if (id):
-        return filter(
-        lambda i:
-        int(i['Id']) == int(id), prices)
+    rowFile = open('./../data/sales.json')
+    sales = json.load(rowFile)
+    rowFile = open('./../data/priceChanges.json')
+    priceChanges = json.load(rowFile)
 
-    return prices
+    @staticmethod
+    def findNextID(arr):
+        iDs = map(lambda i: i['Id'], arr)
+        maxId = max(iDs)
 
-def getsales(id):
-  rowFile = open('./../data/sales.json')
-  sales = json.load(rowFile)
-  if (id):
-      return filter(
-          lambda i:
-          str(i['Id']) == id, sales)
+        return maxId + 1
 
-  return sales
+    @staticmethod
+    def getprices(id):
+        if (id):
+            return filter(
+            lambda i:
+            int(i['Id']) == int(id), MemoryDB.prices)
+        return MemoryDB.prices
 
-def getPriceChanges(id):
-  rowFile = open('./../data/priceChanges.json')
-  priceChanges = json.load(rowFile)
+    @staticmethod
+    def upsertprices(data):
+        if (data.get('Id') == None):
+            data['Id'] = MemoryDB.findNextID(MemoryDB.prices)
+        else:
+            MemoryDB.deleteprices(data['Id'])
+        MemoryDB.prices.append(data)
 
-  if (id):
-      return filter(
-          lambda i:
-          str(i['Id']) == id, priceChanges)
+    @staticmethod
+    def deleteprices(id):
+        MemoryDB.prices = filter(lambda i: i['Id'] != int(id), MemoryDB.prices)
 
-  return priceChanges
+    @staticmethod
+    def getsales(id):
+      if (id):
+          return filter(
+              lambda i:
+              str(i['Id']) == id, MemoryDB.sales)
+      return MemoryDB.sales
 
+    @staticmethod
+    def upsertsales(data):
+        if (data.get('Id') == None):
+            data['Id'] = MemoryDB.findNextID(MemoryDB.sales)
+        else:
+            MemoryDB.deletesales(data['Id'])
+        MemoryDB.sales.append(data)
+
+    @staticmethod
+    def deletesales(id):
+        MemoryDB.sales = filter(lambda i: i['Id'] != int(id), MemoryDB.sales)
+
+    @staticmethod
+    def getPriceChanges(id):
+      if (id):
+          return filter(
+              lambda i:
+              str(i['Id']) == id, MemoryDB.priceChanges)
+      return MemoryDB.priceChanges
