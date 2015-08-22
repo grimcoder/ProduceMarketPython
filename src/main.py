873e72348496c56e66c6f2d23dacbeaf47dad698
Taras Kovtun <grimcoder@users.noreply.github.com>
2015-08-22 00:46:09 -0700

    started mongoDB persistence layer

--- src/main.py
+++ src/main.py
@@ -4,7 +4,9 @@ from flask.ext.cors import CORS
 import sys
 
 if ('m' in sys.argv):
-    useMongo = True
+    from memorydb.DB import *
+else:
+    from mongoDB.DB import *
 
 app = FlaskAPI(__name__)
 CORS(app)
@@ -13,42 +15,42 @@ from memorydb.DB import *
 @app.route("/api/prices", methods=['GET'])
 def prices():
     id = request.args.get('id')
-    return MemoryDB.getprices(id)
+    return DB.getprices(id)
 
 @app.route("/api/prices", methods=['POST'])
 def upsertprices():
     data = request.data
-    MemoryDB.upsertprices(data)
+    DB.upsertprices(data)
     return ''
 
 
 @app.route("/api/sales", methods=['POST'])
 def upsertsales():
     data = request.data
-    MemoryDB.upsertsales(data)
+    DB.upsertsales(data)
     return ''
 
 @app.route("/api/prices", methods=['DELETE'])
 def pricesdelete():
     id = request.args.get('id')
-    MemoryDB.deleteprices(id)
+    DB.deleteprices(id)
     return ''
 
 @app.route("/api/sales")
 def sales():
     id = request.args.get('id')
-    return MemoryDB.getsales(id)
+    return DB.getsales(id)
 
 @app.route("/api/sales", methods=['DELETE'])
 def salesdelete():
     id = request.args.get('id')
-    MemoryDB.deletesales(id)
+    DB.deletesales(id)
     return ''
 
 @app.route("/api/reports/prices")
 def pricechanges():
     id = request.args.get('id')
-    return MemoryDB.getPriceChanges(id)
+    return DB.getPriceChanges(id)
 
 if __name__ == '__main__':
     app.run(debug=True, port=3001, threaded=True)
--- src/mongoDB/DB.py
+++ src/mongoDB/DB.py
@@ -4,14 +4,15 @@ import pymongo
 
 import json
 
-class MemoryDB(object):
 
-    # rowFile = open('./../data/prices.json')
-    # prices = json.load(rowFile)
-    # rowFile = open('./../data/sales.json')
-    # sales = json.load(rowFile)
-    # rowFile = open('./../data/priceChanges.json')
-    # priceChanges = json.load(rowFile)
+class DB(object):
+
+    rowFile = open('./../data/prices.json')
+    prices = json.load(rowFile)
+    rowFile = open('./../data/sales.json')
+    sales = json.load(rowFile)
+    rowFile = open('./../data/priceChanges.json')
+    priceChanges = json.load(rowFile)
 
     @staticmethod
     def findNextID(arr):
@@ -31,64 +32,64 @@ class MemoryDB(object):
         if (id):
             return filter(
             lambda i:
-            int(i['Id']) == int(id), MemoryDB.prices)
-        return MemoryDB.prices
+            int(i['Id']) == int(id), DB.prices)
+        return DB.prices
 
     @staticmethod
     def upsertprices(data):
         oldPrice = 'n/a'
         if (data.get('Id') == None):
             Action = 'New'
-            data['Id'] = MemoryDB.findNextID(MemoryDB.prices)
+            data['Id'] = DB.findNextID(DB.prices)
         else:
             Action = 'Edit'
-            oldPrice = filter(lambda i: i['Id'] == int(data['Id']), MemoryDB.prices)[0]['Price']
-            MemoryDB.prices = filter(lambda i: i['Id'] != int(data['Id']), MemoryDB.prices)
+            oldPrice = filter(lambda i: i['Id'] == int(data['Id']), DB.prices)[0]['Price']
+            DB.prices = filter(lambda i: i['Id'] != int(data['Id']), DB.prices)
 
-        MemoryDB.prices.append(data)
+        DB.prices.append(data)
         data['Action'] = Action
         data['priceWas'] = oldPrice
-        MemoryDB.priceChanges.append(data)
-        MemoryDB.saveobjecttofile(MemoryDB.prices,'./../data/prices.json')
-        MemoryDB.saveobjecttofile(MemoryDB.priceChanges,'./../data/priceChanges.json')
+        DB.priceChanges.append(data)
+        DB.saveobjecttofile(DB.prices,'./../data/prices.json')
+        DB.saveobjecttofile(DB.priceChanges,'./../data/priceChanges.json')
 
 
     @staticmethod
     def deleteprices(id):
 
-        data = filter(lambda i: i['Id'] == int(id), MemoryDB.prices)[0]
-        MemoryDB.prices = filter(lambda i: i['Id'] != int(id), MemoryDB.prices)
-        MemoryDB.saveobjecttofile(MemoryDB.prices,'./../data/prices.json')
+        data = filter(lambda i: i['Id'] == int(id), DB.prices)[0]
+        DB.prices = filter(lambda i: i['Id'] != int(id), DB.prices)
+        DB.saveobjecttofile(DB.prices,'./../data/prices.json')
         data['Action'] = "Delete"
-        MemoryDB.priceChanges.append(data)
-        MemoryDB.saveobjecttofile(MemoryDB.priceChanges,'./../data/priceChanges.json')
+        DB.priceChanges.append(data)
+        DB.saveobjecttofile(DB.priceChanges,'./../data/priceChanges.json')
 
     @staticmethod
     def getsales(id):
       if (id):
           return filter(
               lambda i:
-              str(i['Id']) == id, MemoryDB.sales)
-      return MemoryDB.sales
+              str(i['Id']) == id, DB.sales)
+      return DB.sales
 
     @staticmethod
     def upsertsales(data):
         if (data.get('Id') == None):
-            data['Id'] = MemoryDB.findNextID(MemoryDB.sales)
+            data['Id'] = DB.findNextID(DB.sales)
         else:
-            MemoryDB.deletesales(data['Id'])
-        MemoryDB.sales.append(data)
-        MemoryDB.saveobjecttofile(MemoryDB.sales,'./../data/sales.json')
+            DB.deletesales(data['Id'])
+        DB.sales.append(data)
+        DB.saveobjecttofile(DB.sales,'./../data/sales.json')
 
     @staticmethod
     def deletesales(id):
-        MemoryDB.sales = filter(lambda i: i['Id'] != int(id), MemoryDB.sales)
-        MemoryDB.saveobjecttofile(MemoryDB.sales,'./../data/sales.json')
+        DB.sales = filter(lambda i: i['Id'] != int(id), DB.sales)
+        DB.saveobjecttofile(DB.sales,'./../data/sales.json')
 
     @staticmethod
     def getPriceChanges(id):
       if (id):
           return filter(
               lambda i:
-              str(i['Id']) == id, MemoryDB.priceChanges)
-      return MemoryDB.priceChanges
+              str(i['Id']) == id, DB.priceChanges)
+      return DB.priceChanges
